252c2143291f02b5380de8dd89a8757eff29ff81
Han Yanqing <hanyanqing@100tal.com>
2017-10-28 22:52:54 +0800

    make large the page number

--- src/main/java/com/netease/course/controller/IndexController.java
+++ src/main/java/com/netease/course/controller/IndexController.java
@@ -19,7 +19,8 @@ public class IndexController {
 	ContentsService contentsService;
 
 	/**
-	 * 首页，增加所有内容的分页；未购买页面，由于需要改动比较大，未做分页处理；
+	 * 首页，增加所有内容的分页未购买页面，由于需要改动比较大，未做分页处理；
+	 * TODO 【js注释了删除跳转代码，由于分页有一个小问题当前就是页面只剩一个再删除，需跳转页码】；
 	 * 
 	 * @author want
 	 * @param map
@@ -33,7 +34,7 @@ public class IndexController {
 			@RequestParam(required = false, defaultValue = "1") int pageNO,@RequestParam(required = false) Integer type) {
 		Person user = (Person) session.getAttribute("user");
 		System.out.println("index");
-		int size = 1;
+		int size = 10;
 		model.addAttribute("size", size);
 		model.addAttribute("pageNO", pageNO);
 		model.addAttribute("count", contentsService.getContentsCount());
